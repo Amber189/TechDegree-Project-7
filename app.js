48aa66775b129da6420d21e10fd56837dc68829f
Amber <32441978+Amber189@users.noreply.github.com>
2018-05-24 21:07:53 -0500

    get random array function

--- app.js
+++ app.js
@@ -1,35 +1,97 @@
-// $('.btn__reset').click.hide();
-
 let keys = document.getElementById('qwerty');
 let phrase = document.getElementById('phrase');
 let missed = 0;
 let startGame = document.querySelector('a.btn__reset');
 
-let phrases  = ['Necessity is the mother of invention',
-                'Nip it in the bud',
-                'You can not make an omelet without breaking a few eggs',
-                'Practice makes perfect',
-                'Good things come to those who wait'];
-
-
 //hide div "overlay" when "btn__reset" is clicked
-
-// function hideOverlay () {
-//   startGame.style.display = "hidden";
-// }
 
 startGame.addEventListener('click', () => {
 document.getElementById('overlay').style.display = "none";
-
 
 });
 
 
 
-// get random phrase
+// randomly choose a phrase from the phrases array and split that phrase into a new array of characters
 
-// function getRandomPhraseAsArray ()
+// let phrases  = ['Necessity is the mother of invention',
+//                 'Nip it in the bud',
+//                 'You can not make an omelet without breaking a few eggs',
+//                 'Practice makes perfect',
+//                 'Good things come to those who wait'];
+// let randomNumber =  Math.floor (Math.random() * phrases.length) + 1;
+//
+// function getRandomPhraseAsArray (){
+//
+//                       return getRandomPhraseAsArray(phrases[randomNumber]).split("");
+//                     }
+
+
+   let phrases  = ['Necessity is the mother of invention',
+                   'Nip it in the bud',
+                   'You can not make an omelet without breaking a few eggs',
+                   'Practice makes perfect',
+                   'Good things come to those who wait'];
+
+
+
+
+
+
+
+
+ function getRandomPhraseAsArray (phrases) {
+  return phrases[(Math.floor (Math.random() * phrases.length) + 1)].split("");
+
+
+}
+ getRandomPhraseAsArray(phrases);
+//
+// function getRandomPhraseAsArray (){
+//
+//                       let phrases  = ['Necessity is the mother of invention',
+//                                       'Nip it in the bud',
+//                                       'You can not make an omelet without breaking a few eggs',
+//                                       'Practice makes perfect',
+//                                       'Good things come to those who wait'];
+//                       let randomNumber =  Math.floor (Math.random() * phrases.length) + 1;
+//
+//                       return(phrases[randomNumber]);
+//
+//                     }
+//
+//
+// let phraseSplit = getRandomPhraseAsArray().split("");
+
+
 
 
 
 // set game display
+function addPhraseToDisplay(arr) {
+  for (i= 0; i <= phraseSplit.length; i++) {
+    let node = document.createElement('LI');
+    let letterNode = document.createTextNode(phraseSplit);
+    node.appendChild(letterNode);
+    document.querySelector('#phrase ul').appendChild(node);
+  }
+
+}
+
+addPhraseToDisplay(phraseSplit);
+
+
+
+
+
+// function addPhraseToDisplay() {
+//   for (i= 0; i <= phraseSplit.length; i++) {
+//     let node = document.createElement('LI');
+//     let letterNode = document.createTextNode(phraseSplit);
+//     node.appendChild(letterNode);
+//     document.querySelector('#phrase ul').appendChild(node);
+//   }
+//
+// }
+//
+// addPhraseToDisplay(phraseSplit);
